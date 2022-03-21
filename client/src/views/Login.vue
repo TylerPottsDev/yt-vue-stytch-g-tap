d77528ff731ac2f85edeb2dc699cc46f5b2598c4
Tyler Potts <tylerpottsdev@gmail.com>
2022-03-21 01:15:09 +0000

    Stytch G One Tap

---
+++ client/src/views/Login.vue
@@ -0,0 +1,49 @@
+<script setup>
+import {loadStytch} from '@stytch/stytch-js'
+import { onMounted } from 'vue'
+
+const setup = async () => {
+	const Stytch = await loadStytch()
+
+	let stytch = Stytch("public-token-test-2e21111b-02b5-4c03-8815-a01353e5e3f3", {
+		elementId: "#gonetap",
+		loginOrSignupView: {
+			products: ['oauth'],
+			oauthOptions: {
+				providers: [{type: 'google', 'one_tap': true, position: 'embedded'}]
+			},
+		},
+		style: {
+			fontFamily: 'Arial',
+			width: '321px',
+			primaryColor: '#106ee9',
+		},
+		publicToken: "public-token-test-2e21111b-02b5-4c03-8815-a01353e5e3f3",
+		callbacks: {
+			onEvent: (data) => {
+				console.log(data);
+			},
+			onSuccess: (data) => console.log(data),
+			onError: (data) => console.log(data),
+		},
+	})
+}
+
+onMounted(() => {
+	setup()
+})
+</script>
+
+<template>
+	<main class="login min-h-screen bg-gradient-to-br from-green-500 to-cyan-400">
+		<section class="container mx-auto pt-16 pb-4 px-4">
+			
+			<h1 class="text-4xl mb-8 text-center font-black uppercase">Password-less Login</h1>
+
+			<div class="bg-gray-700 max-w-md mx-auto rounded p-4">
+				<div id="gonetap"></div>
+			</div>
+
+		</section>
+	</main>
+</template>
---
+++ server/server.js
@@ -0,0 +1,91 @@
+// SETUP
+const express = require("express")
+const stytch = require("stytch")
+const app = express()
+require("dotenv").config()
+
+const client = new stytch.Client({
+	project_id: process.env.STYTCH_PID,
+	secret: process.env.STYTCH_SECRET,
+	env: process.env.STYTCH_ENV === "test" 
+		? stytch.envs.test 
+		: stytch.envs.live
+})
+// END OF SETUP
+
+// MIDDLEWARE
+app.use(express.json())
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*")
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+	next()
+})
+// END OF MIDDLEWARE
+
+// ROUTES
+app.get('/auth', async (req, res) => {
+	const { token } = req.query
+
+	// console.log(token);
+
+	try {
+		const response = await client.oauth.authenticate(token, {
+			session_management_type: 'stytch',
+			session_duration_minutes: 5
+		})
+
+		res.redirect(`http://localhost:3000?session_token=${response.session.stytch_session.session_token}`)
+	} catch (err) {
+		console.log(err)
+
+		res.status(400).json({
+			success: false,
+			message: "Error authenticating",
+			error: err
+		})
+	}
+	
+})
+
+app.post('/verify', async (req, res) => {
+	const { token } = req.body
+
+	try {
+		await client.sessions.authenticate({session_token: token})
+
+		res.status(200).json({
+			success: true,
+			message: "Session Verified"
+		})
+	} catch(err) {
+		console.log(err)
+		
+		res.status(400).json({
+			success: false,
+			message: "Error authenticating",
+			error: err
+		})
+	}
+})
+
+app.get('/reset/:id', (req, res) => {
+	client
+		.users
+		.delete(req.params.id)
+		.then(resp => {
+			console.log(resp)
+			res.send("User Deleted")
+		})
+		.catch(err => {
+			res.send("An error occured")
+			console.log(err)
+		});
+})
+// END OF ROUTES
+
+// INITIALIZE
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Server listening on port ${port}`)
+})
+// END OF INITIALIZE
